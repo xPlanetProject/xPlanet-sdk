85dd4b04ac2b736bd585e45356be75ed3e04a974
rwson <rw_song@sina.com>
2021-05-27 13:36:44 +0800

    modify some address

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x422Fa5C8c789Ac8f34C6B7815cd5dbf2C60901ef";
+export declare const INIT_CODE_HASH = "0x999166400106f530b7aa5a4c7160878495a12e0f831d3b89dd0e3a3b10f86eca";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
